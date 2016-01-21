18cbb8d85617c20fcb2514df2e0915d9402d0451
Temuulen1 <temuulen_800@yahoo.com>
2016-01-21 13:22:25 -0800

    The END!

--- from selenium import webdriver.py
+++ from selenium import webdriver.py
@@ -2,7 +2,9 @@ import time
 from selenium import webdriver
 print ('Hello, enter the email you want to send the message to')
 userEmail = input('')
-print ('What is your message?')
+print ('What would you like your email subject to be?')
+userSubject = input('')
+print ('Type your message please')
 userText = input('')
 
 browser = webdriver.Firefox()
@@ -13,13 +15,13 @@ emailElem = browser.find_element_by_id('Email')
 emailElem.send_keys('temuulen1234567@gmail.com')
 emailElem.submit()
 
-time.sleep(3)
+time.sleep(2)
 
 passwordElem = browser.find_element_by_id('Passwd')
 passwordElem.send_keys('Quality1234567')
 passwordElem.submit()
 
-time.sleep(7)
+time.sleep(5)
 
 composeElem = browser.find_element_by_class_name('z0')
 composeElem.click()
@@ -29,12 +31,18 @@ time.sleep(3)
 toElem = browser.find_element_by_class_name('vO')
 toElem.send_keys(userEmail)
 
-time.sleep(3)
+time.sleep(1)
 
-subjElem = browser.find_element_by_class_name('Am.Al.editable.LW-avf')
-subjElem.send_keys(usertext)
+toElem = browser.find_element_by_class_name('aoT')
+toElem.send_keys(userSubject)
 
 
+bodyElem = browser.find_element_by_class_name('Am')
+bodyElem.send_keys(userText)
 
+time.sleep(2)
+
+sendElem = browser.find_element_by_xpath("//div[@class='J-J5-Ji']/div[@class='T-I J-J5-Ji aoO T-I-atl L3']")
+sendElem.click()
 
 
